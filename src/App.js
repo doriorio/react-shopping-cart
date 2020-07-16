38a76f1141774bd91fbe79b3e8e2d88633e15cd8
doriorio <itsdori@gmail.com>
2020-07-15 21:22:40 -0700

    cart component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import data from "./data.json"
 import Products from './components/Products';
 import Filter from './components/Filter';
+import Cart from './components/Cart';
 
 
 class App extends React.Component {
@@ -10,18 +11,40 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size:"",
       sort: "",
     }
   };
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({cartItems: 
+      cartItems.filter((x) => x._id !== product._id),
+
+    })
+  }
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if (item._id === product.__id){
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count: 1 });
+    }
+    this.setState({cartItems});
+  }
   sortProducts = (evt) => {
     const sort = evt.target.value;
     this.setState(state =>({
       sort: sort,
       products: this.state.products.slice().sort((a, b) => (
-        sort == 'lowest'?
+        sort === 'lowest'?
          ((a.price > b.price)? 1:-1):
-         sort == 'highest'?
+         sort === 'highest'?
          ((a.price < b.price)? 1:-1):
          ((a._id < b._id)? 1:-1)
       ))
@@ -30,7 +53,7 @@ class App extends React.Component {
     };
     filterProducts = (evt) => {
       console.log(evt.target.value);
-      if (evt.target.value == ''){
+      if (evt.target.value === ''){
         this.setState({
           size:evt.target.value, 
           products:data.products
@@ -49,7 +72,6 @@ class App extends React.Component {
     
   }
   render() {
-
     return (
       <div className="grid-container">
         <header>
@@ -58,28 +80,26 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Filter 
-              count={this.state.products.length}
-              size={this.state.size}
-              sort={this.state.sort}
-              filterProducts={this.filterProducts}
-              sortProducts={this.sortProducts}
-              >
-              </Filter>
-              <Products products={this.state.products}></Products>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
+              <Products products={this.state.products}
+              addToCart={this.addToCart}
+              ></Products>
             </div>
-              <div className="sidebar">
-                Cart Items
-              </div>
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems}
+              removeFromCart={this.removeFromCart} />
+            </div>
           </div>
         </main>
-        <footer>
-          All rights reserved
-        </footer>
-  
+        <footer>All right is reserved.</footer>
       </div>
     );
   }
 }
-
 export default App;
