3a7b342db6ba9af7f992adbd8ce14540406efba0
doriorio <itsdori@gmail.com>
2020-07-23 20:56:13 -0700

    checkout form

--- src/App.js
+++ src/App.js
@@ -11,23 +11,29 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size:"",
       sort: "",
-    }
-  };
+    };
+  }
+  createOrder = (order => {
+    alert("need to save order for" + order.name)
+  })
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({cartItems: 
       cartItems.filter((x) => x._id !== product._id),
-
-    })
-  }
+    });
+    localStorage.setItem(
+      "cartItems",
+       JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
+  };
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     let alreadyInCart = false;
     cartItems.forEach(item => {
-      if (item._id === product.__id){
+      if (item._id === product._id){
         item.count++;
         alreadyInCart = true;
       }
@@ -35,7 +41,8 @@ class App extends React.Component {
     if(!alreadyInCart){
       cartItems.push({...product, count: 1 });
     }
-    this.setState({cartItems});
+    this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
   sortProducts = (evt) => {
     const sort = evt.target.value;
@@ -92,7 +99,9 @@ class App extends React.Component {
               ></Products>
             </div>
             <div className="sidebar">
-              <Cart cartItems={this.state.cartItems}
+              <Cart 
+              createOrder={this.createOrder}
+              cartItems={this.state.cartItems}
               removeFromCart={this.removeFromCart} />
             </div>
           </div>
